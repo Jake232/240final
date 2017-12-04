d8e07d3538a01c09ee314e3865ec3d88cd4bb77a
Jake Braswell <jakebraswell232@gmail.com>
2017-12-04 17:55:21 -0500

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -5,29 +5,33 @@ using namespace std;
 void world::move(human h){
 		if(!h.dead){
 		int direction = rand()&4;
-		int x = Human.getX();
-		int y = Human.getY();
+		int x = h.getX();
+		int y = h.getY();
 		if(direction == 0){
 			if(x!=7){
-				if(h.move(World[++x,y]))
+				char *a = World[++x,y];
+				if(h.move(*a))
 					h.setX(++x);
 			}
 		}
 		else if(direction == 1){
 			if(y!=7){
-				if(h.move(World[x,++y]))
+			char *a = World[x,++y];
+				if(h.move(*a))
 					h.setY(++y);
 			}
 		}
 		else if(direction == 2){
 			if(x!=0){
-				if(h.move(World[--x,y]))
+				char *a = World[--x,y];
+				if(h.move(*a))
 					h.setX(--x);
 			}
 		}
 		else{
 			if(y!=0){
-				if(h.move(World[x,--y]))
+				char *a = World[x,--y];
+				if(h.move(*a))
 					h.setY(--y);
 			}
 		}
@@ -37,29 +41,33 @@ void world::move(human h){
 void world::move(zombie z){
 	if(!z.dead){
 		int direction = rand()&4;
-		int x = Human.getX();
-		int y = Human.getY();
+		int x = z.getX();
+		int y = z.getY();
 		if(direction == 0){
 			if(x!=7){
-				if(z.move(World[++x,y]))
+				char *a = World[++x,y];
+				if(z.move(*a))
 					z.setX(++x);
 			}
 		}
 		else if(direction == 1){
 			if(y!=7){
-				if(z.move(World[x,++y]))
+				char *a = World[x,++y];
+				if(z.move(*a))
 					z.setY(++y);
 			}
 		}
 		else if(direction == 2){
 			if(x!=0){
-				if(z.move(World[--x,y]))
+				char *a = World[--x,y];
+				if(z.move(*a))
 					z.setX(--x);
 			}
 		}
 		else{
 			if(y!=0){
-				if(z.move(World[x,--y]))
+				char *a = World[x,--y];
+				if(z.move(*a))
 					z.setY(--y);
 			}
 		}
@@ -71,7 +79,7 @@ void world::move(zombie z){
 void world::print(){
 	for(int i=0; i<8; i++){
 		for(int j=0; j<8; j++){
-			switch(world[i][j]){
+			switch(World[i][j]){
 				case 'H':
 					cout << "| H ";
 					break;
@@ -79,7 +87,7 @@ void world::print(){
 					cout << "| Z ";
 					break;
 				case ' ':
-					cout << "|  "
+					cout << "|  ";
 					break;
 				default:
 					cout << "Uhh thats not good..." << endl;
@@ -99,23 +107,24 @@ void world::setUp(){
 		int x = rand()%8;
 		int y = rand()%8;
 		if(!anyHumansHere(x,y)){
-			human a = new human(x,y,false);
+			human *a = new human(x,y,false);
 			++numHumans;
-			Human[numHumans] = a;
+			Humans[numHumans] = *a;
 		}
 		else
 			--i;
 }
+}
 bool world::anyHumansHere(int x, int y){
 	for(int i=0; i<8; i++){
-		if(Human[i].getX==x&&Human[i].getY==y)
+		if(Humans[i].getX()==x&&Humans[i].getY()==y)
 		return true;
 	}
 return false;
 }
 bool world::anyZombiesHere(int x, int y){
 	for(int i=0; i<9; i++){
-		if(Zombie[i].getX==x && Zombie[i].getY==y)
+		if(Zombies[i].getX()==x && Zombies[i].getY()==y)
 		return true;
 	}
 return false;
@@ -129,4 +138,6 @@ void world::refreshWorld(){
 				World[i][j] = 'Z';
 			else
 				World[i][j] = ' ';
+		}
+	}
 }
