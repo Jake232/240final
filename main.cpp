4332e0f1cb310c05c36b60a6d09e8a629954de72
Jake Braswell <jakebraswell232@gmail.com>
2017-12-05 13:06:22 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include "world.cpp"
 int main(int argc, char **argv){
-world god = new world();
+world god;
 bool allDead = false;
-int day = 0;
-while(!allDead){
-	cout << "Day: " << day << endl;
-		
-	allDead=true;
-	for(int i=0; i<8; i++){
-		if(god.Humans[i].dead==false)
-			allDead = true;
-		}
-		++day;
-	}
+int day = 1;
+god.setUp();
+//while(!allDead){
+cout << "Day: " << day << endl;
+god.refreshWorld();
+god.print();
+
+for(int i=0; i<8; i++){
+god.move(god.Humans[i]);
+}
+
+allDead=true;
+for(int i=0; i<8; i++){
+	if(god.Humans[i].dead==false)
+		allDead = true;
+}
+	++day;
+//	}
 }
