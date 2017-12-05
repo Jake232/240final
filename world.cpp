e14aa437926d5e3f3357ffd243cce27745a20b01
Jake Braswell <jakebraswell232@gmail.com>
2017-12-05 13:08:12 -0500

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -2,43 +2,80 @@
 #include<stdlib.h>
 #include<iostream>
 using namespace std;
-void world::move(human h){
+world::world(){
+	numHumans = 0;
+	numZombies = 0;
+}
+void world::move(human &h){
 		if(!h.dead){
-		int direction = rand()&4;
+		int direction = rand()&4-1;
 		int x = h.getX();
 		int y = h.getY();
 		if(direction == 0){
 			if(x!=7){
-				char *a = World[++x,y];
-				if(h.move(*a))
-					h.setX(++x);
+				x+=1;
+				if(World[x][y]=='Z'){
+					h.setX(-1);
+					h.setY(-1);
+					zombie *a = new zombie(x,y,true);
+					Zombies[numZombies] = *a;
+					World[x][y] = 'Z';
+				}
+				else if(h.move(World[x][y])){
+					h.setX(x);
+					World[x][y]='H';
 			}
 		}
+	}
 		else if(direction == 1){
 			if(y!=7){
-			char *a = World[x,++y];
-				if(h.move(*a))
-					h.setY(++y);
+			y+=1;
+			if(World[x][y]=='Z'){
+					h.setX(-1);
+					h.setY(-1);
+					zombie *a = new zombie(x,y,true);
+					Zombies[numZombies] = *a;
+					World[x][y] = 'Z';
+				}
+				else if(h.move(World[x][y]))
+					h.setY(y);
+					World[x][y]='H';
 			}
 		}
 		else if(direction == 2){
 			if(x!=0){
-				char *a = World[--x,y];
-				if(h.move(*a))
-					h.setX(--x);
+				x-=1;
+				if(World[x][y]=='Z'){
+					h.setX(-1);
+					h.setY(-1);
+					zombie *a = new zombie(x,y,true);
+					Zombies[numZombies] = *a;
+					World[x][y] = 'Z';
+				}
+				else if(h.move(World[x][y]))
+					h.setX(x);
+					World[x][y]='H';
 			}
 		}
 		else{
 			if(y!=0){
-				char *a = World[x,--y];
-				if(h.move(*a))
-					h.setY(--y);
+				y-=1;
+				if(World[x][y]=='Z'){
+					h.setX(-1);
+					h.setY(-1);
+					zombie *a = new zombie(x,y,true);
+					Zombies[numZombies] = *a;
+					World[x][y] = 'Z';
+				}
+				else if(h.move(World[x][y]))
+					h.setY(y);
+					World[x][y]='H';
 			}
 		}
 		
 	}
 }
-void world::move(zombie z){
+void world::move(zombie &z){
 	if(!z.dead){
 		int direction = rand()&4;
 		int x = z.getX();
@@ -79,30 +116,34 @@ void world::move(zombie z){
 void world::print(){
 	for(int i=0; i<8; i++){
 		for(int j=0; j<8; j++){
+			cout << "|";
 			switch(World[i][j]){
 				case 'H':
-					cout << "| H ";
+					cout << "H";
 					break;
 				case 'Z':
-					cout << "| Z ";
+					cout << "Z";
 					break;
 				case ' ':
-					cout << "|  ";
+					cout << " ";
 					break;
 				default:
 					cout << "Uhh thats not good..." << endl;
 					break;			
 			}
-			cout << "|" << endl;
 		}
+		cout << "|" <<endl;
 	}
+	cout << endl;
 }
 
 void world::setUp(){
 	for(int i=0; i<8; i++){
-		for(int j=0; j<8; j++)
+		for(int j=0; j<8; j++){
 		World[i][j] = ' ';
+		}
 	}
+	
 	for(int i=0; i<8; i++){
 		int x = rand()%8;
 		int y = rand()%8;
@@ -113,11 +154,29 @@ void world::setUp(){
 		}
 		else
 			--i;
-}
+	}
+	for(int i=0; i<9; i++){
+		Zombies[i].setX(-1);
+		Zombies[i].setY(-1);
+	}
+	for(int i=0; i<1; i++){
+		int x = rand()%8;
+		int y = rand()%8;
+		if(!anyHumansHere(x,y)){
+			zombie *a = new zombie(x,y,true);
+			++numZombies;
+			Zombies[0] = *a;
+			Zombies[1].setX(-1);
+			Zombies[1].setY(-1);
+		}
+		else{
+			--i;
+		}
+	}
 }
 bool world::anyHumansHere(int x, int y){
 	for(int i=0; i<8; i++){
-		if(Humans[i].getX()==x&&Humans[i].getY()==y)
+		if(Humans[i].getX()==x && Humans[i].getY()==y)
 		return true;
 	}
 return false;
